151be45e7f071d2ea1f5e24d678dddf758b965e1
abikoraj <abikoraj@gmail.com>
2024-08-03 06:17:42 +0545

    Web: UI components enhanced

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\PostCategoryController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('post.list');
 });
+
+Route::get('/post/list', [PostController::class, 'list'])->name('post.list');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -15,6 +19,17 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('post-categories')->name('post-categories.')->group(function () {
+        Route::get('/', [PostCategoryController::class, 'index'])->name('index');
+        Route::get('/create', [PostCategoryController::class, 'create'])->name('create');
+        Route::post('/', [PostCategoryController::class, 'store'])->name('store');
+        Route::get('/{postCategory}/edit', [PostCategoryController::class, 'edit'])->name('edit');
+        Route::patch('update/{postCategory}', [PostCategoryController::class, 'update'])->name('update');
+        Route::get('delete/{postCategory}', [PostCategoryController::class, 'destroy'])->name('destroy');
+    });
+
+    Route::resource('posts', PostController::class);
 });
 
 require __DIR__.'/auth.php';
